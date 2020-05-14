907b073708e73c5b9e5d850768f818bad3cac6a2
Mojtaba Javan <javan.mojtaba@gmail.com>
2020-05-15 00:27:41 +0430

    Change icons and popup

--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    console.log("Nothing in here!");
+    let links = document.getElementsByTagName("a");
+    for (let link of links) {
+      let location = link.href;
+      link.onclick = function () {
+        chrome.tabs.create({ active: true, url: location });
+      };
+    }
   },
   false
 );
